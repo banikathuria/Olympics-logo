7c80e5412f2787c2ad9597ea606276d206ef3bcc
banikathuria <79353266+banikathuria@users.noreply.github.com>
2021-10-30 14:07:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+canvas= document.getElementById("myCanvas");
+ var ctx= canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle="blue"
+ctx.lineWidth=5
+ctx.arc(150,300,75,0,360)
+ctx.stroke()
+canvas= document.getElementById("myCanvas");
+ var ctx= canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle="black"
+ctx.lineWidth=5
+ctx.arc(350,300,75,0,360)
+ctx.stroke()
+canvas= document.getElementById("myCanvas");
+ var ctx= canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle="red"
+ctx.lineWidth=5
+ctx.arc(550,300,75,0,360)
+ctx.stroke()
+canvas= document.getElementById("myCanvas");
+ var ctx= canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle="yellow"
+ctx.lineWidth=5
+ctx.arc(250,385,75,0,360)
+ctx.stroke()
+canvas= document.getElementById("myCanvas");
+ var ctx= canvas.getContext("2d")
+ctx.beginPath()
+ctx.strokeStyle="green"
+ctx.lineWidth=5
+ctx.arc(450,385,75,0,360)
+ctx.stroke()
